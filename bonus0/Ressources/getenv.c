8de9483476a128c4541df6cf01cf213175c6b157
Arthur Woimbée <arthur.woimbee@gmail.com>
2020-10-17 23:00:38 +0200

    improve getenv

--- bonus0/Ressources/getenv.c
+++ bonus0/Ressources/getenv.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void main(int ac, char *av[])
 {
 	if (ac != 2) {
 		printf("Usage: %s [env_var]\n", av[0]);
 	} else {
-		printf("%s @ %p\n", av[1], getenv(av[1]));
+		char *val = getenv(av[1]);
+		char hex[sizeof(void*)*2 + 3] = {0};
+		sprintf(hex, "%p", val);
+		char embedable[sizeof(void*)*4 + 1] = {0};
+		if (val)
+		{
+			char *p = embedable;
+			char *end = strchr(hex, 0);
+			while (end > &hex[2]) {
+				end -= 2;
+				p[0] = '\\';
+				p[1] = 'x';
+				p[2] = end[0];
+				p[3] = end[1];
+				p += 4;
+			}
+		}
+		printf("%s @ %s -> %s\n", av[1], hex, embedable);
 	}
 }
